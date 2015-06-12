bd0d85f473832ae0b059697cebba13eba0b84b14
Gal Pasternak <galman33@gmail.com>
2015-06-12 19:29:16 +0300

    Add particles to blackroom

---
+++ Assets/BlackRoom/BlackRoomParticles.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackRoomParticles : MonoBehaviour {
+
+
+	private float BaseX; private float BaseZ;
+
+
+	public float Mult;
+
+	// Use this for initialization
+	void Start () {
+		BaseX = Perlin.GetRandomBase(); BaseZ = Perlin.GetRandomBase();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = transform.position;
+		pos.x = Perlin.GetSmoothRandom(-10, 10, Mult, BaseX);
+		pos.z = Perlin.GetSmoothRandom(-10, 10, Mult, BaseZ);
+		transform.position = pos;
+	}
+}
